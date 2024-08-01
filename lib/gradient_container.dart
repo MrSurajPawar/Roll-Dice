0f82024a03e8159d58ebb46fc1aff4a6fe7502c4
MrSurajPawar <surajspawar007@gmail.com>
2024-08-01 09:37:52 +0530

    Suraj | new changes done and the button get added to the UI

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/styled_text.dart';
 
 //Declaring the variable for the alignments
 const startAlignment = Alignment.topLeft;
@@ -10,10 +9,19 @@ class GradientContainer extends StatelessWidget {
   // GradientContainer({key}): super(key: key);
   const GradientContainer(this.colorChanger1, this.colorChanger2, {super.key});
 
+  //the class can have multiple constructor seprated by dot(.)
+  const GradientContainer.bgColor({super.key})
+      : colorChanger1 = Colors.deepPurple,
+        colorChanger2 = Colors.indigo;
+
   //Properties
   final Color colorChanger1;
   final Color colorChanger2;
 
+  //methods or function
+  void rollDice() {
+    //....
+  }
   //build(context) method is overridden to describe how to display the widget.
   @override
   Widget build(BuildContext context) {
@@ -25,8 +33,25 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyleText('Hello World!'),
+      child: Center(
+        child: Column(children: [
+          Image.asset(
+            'assets/images/dice-2.png',
+            width: 200,
+          ),
+          TextButton(
+            onPressed: rollDice,
+            child: const Text('Roll the Dice'),
+          ),
+          ElevatedButton(
+            onPressed: rollDice,
+            child: const Text('Button 2'),
+          ),
+          OutlinedButton(
+            onPressed: rollDice,
+            child: const Text('Button 3'),
+          ),
+        ]),
       ),
     );
   }
